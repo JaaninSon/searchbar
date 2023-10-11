44b0e168d81bf89168993c6e68c06fc26d5936e5
JaaninSon <jaan1ove@naver.com>
2023-10-11 21:09:09 +0900

    searchbar 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar placeholder=" search here!" />
     </div>
   );
 }
+
+const SearchBar = styled.input`
+  width: 800px;
+  height: 100px;
+  font-size: 50px;
+`;
 
 export default App;
